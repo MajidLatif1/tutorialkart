83c15e333e5d948f12335b8a3f0146063233ad81
Muhammad Majid Latif <mlmajid.latif1145@gmail.com>
2023-06-28 02:48:39 -0700

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: TutorialKart(),
+    );
+  }
+}
+
+class TutorialKart extends StatelessWidget {
+  const TutorialKart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sample App"),
+      ),
+      body: Material(
+        child: Center(
+            child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                top: 20,
+              ),
+              child: Text(
+                "TutorilKart",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Text(
+                "Sign In",
+                style: TextStyle(color: Colors.grey),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                      keyboardType: TextInputType.name,
+                      decoration: InputDecoration(
+                        labelText: "User Name",
+                        hintText: "User Name",
+                        border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 3, color: Colors.black)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                      padding: EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                      ),
+                      child: TextField(
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(width: 3, color: Colors.black)),
+                        ),
+                      )),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Text(
+                "Forgot Password",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 10,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 400,
+            ),
+            Container(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all(
+                    const Size(460, 50),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Login",
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Does not have account?",
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "Sign in",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
